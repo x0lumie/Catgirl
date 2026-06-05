87bd79d55c3b445df4cf9ca7ebcd61064a08706d
toru <245221436+nazunawtf@users.noreply.github.com>
2026-06-05 19:11:00 +0100

    Update EntityMixin.java

--- src/main/java/lol/catgirl/mixin/EntityMixin.java
+++ src/main/java/lol/catgirl/mixin/EntityMixin.java
@@ -1,15 +1,47 @@
 package lol.catgirl.mixin;
 
+import lol.catgirl.module.movement.MovementFixModule;
 import lol.catgirl.module.movement.NoPushModule;
-import lol.catgirl.utils.IMinecraft;
+import lol.catgirl.utils.player.RotationUtils;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static lol.catgirl.utils.IMinecraft.mc;
+
 
 @Mixin(Entity.class)
-public class EntityMixin implements IMinecraft {
+public abstract class EntityMixin {
+
+
+    @Shadow public abstract float getYRot();
+
+    @Shadow public abstract void setDeltaMovement(Vec3 velocity);
+
+    @Shadow public abstract Vec3 getDeltaMovement();
+
+    @ModifyArgs(
+            method = "moveRelative",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Entity;getInputVector(Lnet/minecraft/world/phys/Vec3;FF)Lnet/minecraft/world/phys/Vec3;"
+            )
+    )
+    private void mf(Args args) {
+        MovementFixModule test = MovementFixModule.INSTANCE;
+        if (test != null && test.isEnabled()) {
+            float customYaw = RotationUtils.getRotationYaw();
+            // args.set(2, customYaw);
+        }
+    }
+
     @Inject(method = "push", at = @At("HEAD"), cancellable = true)
     private void noPush(Entity other, CallbackInfo ci) {
         if ((Object) this == mc.player && NoPushModule.INSTANCE.isEnabled()) {
