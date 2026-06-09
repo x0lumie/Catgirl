c2286d9c2814b109299c606c3f9d1263bedd9527
lumie <143151109+x0lumie@users.noreply.github.com>
2026-06-09 03:04:40 -0500

    blink lag range

--- src/main/java/lol/catgirl/module/ghost/LagRangeModule.java
+++ src/main/java/lol/catgirl/module/ghost/LagRangeModule.java
@@ -7,12 +7,14 @@ import lol.catgirl.event.EventHook;
 import lol.catgirl.event.impl.PostMotionEvent;
 import lol.catgirl.event.impl.Render2DEvent;
 import lol.catgirl.event.impl.Render3DEvent;
+import lol.catgirl.manager.BlinkManager;
 import lol.catgirl.manager.LagManager;
 import lol.catgirl.manager.ModuleManager;
 import lol.catgirl.module.Module;
 import lol.catgirl.module.ModuleCategory;
 import lol.catgirl.module.client.TargetsModule;
 import lol.catgirl.property.impl.BoolProperty;
+import lol.catgirl.property.impl.EnumProperty;
 import lol.catgirl.property.impl.SliderProperty;
 import lol.catgirl.utils.client.MathUtils;
 import lol.catgirl.utils.player.MoveUtils;
@@ -25,15 +27,25 @@ import org.joml.Matrix4f;
 import java.awt.*;
 
 public class LagRangeModule extends Module {
-    public static SliderProperty rangeProperty = new SliderProperty("Range", 3.5f, 0.1f, 6, 0.05f);
-    public static SliderProperty minDelayProperty = new SliderProperty("Min Delay", 50.0f, 0.0f, 5000.0f, 10.0f);
-    public static SliderProperty maxDelayProperty = new SliderProperty("Max Delay", 200.0f, 0.0f, 5000.0f, 10.0f);
-    private final BoolProperty teleports = new BoolProperty("Delay Teleports", true);
-    private final BoolProperty velocity = new BoolProperty("Delay Velocity", true);
-    private final BoolProperty entities = new BoolProperty("Delay Entity Movements", true);
-    public static final BoolProperty displayProperty = new BoolProperty("Display", true);
-    public static final BoolProperty onlyWithKillaura = new BoolProperty("Only With Killaura", true);
-    public static final BoolProperty renderLagPos = new BoolProperty("Render Lag Pos", true);
+
+    public enum Mode {
+        Lag,
+        Blink
+    }
+
+    // so if your confused basically it gets the max range of targets in targets module and
+    // enables blink within that range and disables after you reach <= rangeProperty.
+
+    public EnumProperty<Mode> mode = new EnumProperty<>("Mode", Mode.Lag);
+    public SliderProperty rangeProperty = new SliderProperty("Disable Range", 3.5f, 0.1f, 6, 0.05f);
+    public SliderProperty minDelayProperty = new SliderProperty("Min Delay", 50.0f, 0.0f, 5000.0f, 10.0f).hide(() -> mode.getValue() == Mode.Blink);
+    public SliderProperty maxDelayProperty = new SliderProperty("Max Delay", 200.0f, 0.0f, 5000.0f, 10.0f).hide(() -> mode.getValue() == Mode.Blink);
+    private final BoolProperty teleports = new BoolProperty("Delay Teleports", true).hide(() -> mode.getValue() == Mode.Blink);
+    private final BoolProperty velocity = new BoolProperty("Delay Velocity", true).hide(() -> mode.getValue() == Mode.Blink);
+    private final BoolProperty entities = new BoolProperty("Delay Entity Movements", true).hide(() -> mode.getValue() == Mode.Blink);
+    public final BoolProperty displayProperty = new BoolProperty("Display", true);
+    public final BoolProperty onlyWithKillaura = new BoolProperty("Only With Killaura", true);
+    public final BoolProperty renderLagPos = new BoolProperty("Render Lag Pos", true).hide(() -> mode.getValue() == Mode.Blink);
 
     private boolean lagging = false;
     private int lagAmount = maxDelayProperty.getValue().intValue();
@@ -42,7 +54,7 @@ public class LagRangeModule extends Module {
 
     public LagRangeModule() {
         super("LagRange", "Uses lag in a range to the target to delay your movement on their end.", ModuleCategory.Ghost);
-        addSettings(rangeProperty, minDelayProperty, maxDelayProperty, teleports, velocity, entities, displayProperty, onlyWithKillaura, renderLagPos);
+        addSettings(mode, rangeProperty, minDelayProperty, maxDelayProperty, teleports, velocity, entities, displayProperty, onlyWithKillaura, renderLagPos);
     }
 
     @EventHook
@@ -65,9 +77,13 @@ public class LagRangeModule extends Module {
         double distance = mc.player.distanceTo(TargetsModule.getTarget());
 
         if (distance <= TargetsModule.seekRange.getValue() && distance > rangeProperty.getValue()) {
-            lagAmount = (int) MathUtils.randomInt(minDelayProperty.getValue().intValue(), maxDelayProperty.getValue().intValue());
-            LagManager.spoof(lagAmount, true, velocity.getValue(),
-                    teleports.getValue(), entities.getValue());
+            if (mode.getValue() ==  Mode.Lag) {
+                lagAmount = (int) MathUtils.randomInt(minDelayProperty.getValue().intValue(), maxDelayProperty.getValue().intValue());
+                LagManager.spoof(lagAmount, true, velocity.getValue(),
+                        teleports.getValue(), entities.getValue());
+            } else {
+                BlinkManager.enable();
+            }
             lagging = true;
         } else {
             if (lagging) {
@@ -76,25 +92,24 @@ public class LagRangeModule extends Module {
             return;
         }
 
-        setSuffix(String.format("%.1f | %dms", rangeProperty.getValue(), lagAmount));
+        setSuffix(String.format("%.1f", rangeProperty.getValue()));
     }
+
     @EventHook
     public void onRender2DEvent(Render2DEvent event) {
         if (!displayProperty.getValue() || !lagging) return;
 
-        if (mc.getWindow() == null) return;
-        String text = String.format("Lagging %dms", lagAmount);
-        float fontSize = 18f;
-        float padding = 10f;
-        float textX = (float) (mc.getWindow().getGuiScaledWidth() / 2f - DrawUtil.getStringWidth(text, fontSize) / 2f);
-        float textY = padding + DrawUtil.getFontHeight(fontSize);
+        String text = mode.getValue() == Mode.Lag ? String.format("Lagging %dms", lagAmount) : "Blinking";
+        int textWidth = mc.font.width(text);
+        int x = (event.scaledWidth - textWidth) / 2;
+        int y = 10;
 
-        DrawUtil.drawString(text, textX, textY, fontSize, Color.YELLOW, ResourceManager.getSelectedFont());
+        event.context.drawString(mc.font, text, x, y, Color.YELLOW.getRGB());
     }
 
     @EventHook
     public void onRender3DEvent(Render3DEvent event) {
-        if (!renderLagPos.getValue() || !lagging) return;
+        if (!renderLagPos.getValue() || !lagging || mode.getValue() == Mode.Blink) return;
         if (!LagManager.hasServerPosition) return;
         if (mc.options.getCameraType().isFirstPerson()) return;
 
@@ -124,8 +139,12 @@ public class LagRangeModule extends Module {
     }
 
     private void disableLag() {
-        LagManager.dispatch();
-        LagManager.disable();
+        if (mode.getValue() == Mode.Lag) {
+            LagManager.dispatch();
+            LagManager.disable();
+        } else {
+            BlinkManager.disable();
+        }
         lagging = false;
     }
 
